458775e323d531c80014ba917a4d6722ba4324bf
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2025-01-22 19:08:02 -0700

    Added styles for search text and fixed some formatting

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.textHeader}>Folders</Text>
       <StatusBar style="auto" />
-      <View style={styles.greyBox}/>
+      <View style={styles.greyBox}>
+        <Text style={styles.textSearch}>Search</Text>
+      </View>
       <View style={styles.whiteBox}/>
       <View style={styles.whiteBox}/>
     </View>
@@ -18,9 +20,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#F3F2F8',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    //alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   whiteBox: {
     //flex: 0.5,
@@ -29,7 +32,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     width: 350,
     height: 100,
-    alignItems: 'center',
+    alignSelf: 'center',
     justifyContent: 'space-around',
   },
   greyBox: {
@@ -39,11 +42,21 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     width: 350,
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 20,
   },
   textHeader: {
     fontWeight: 'bold',
     fontSize: 50,
+    marginLeft: 40,
+    marginTop: 100,
+  },
+  textSearch: {
+    fontWeight: 'regular',
+    fontSize: 20,
+    color: '#807F85',
+    marginLeft: 20,
   }
 });

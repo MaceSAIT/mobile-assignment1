9db34df230a15d7833b9577d3a3b19be8236048f
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2025-02-11 14:41:07 -0700

    added stack, then removed them becuase they didnt work

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
+import { Tabs,Stack } from 'expo-router';
 
 export default function TabLayout() {
   return (
+
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
